faea4ac00e62587b1747f9fdd6176d5b94750560
nazir mvuh <nazirmvuh69gmail.com>
2020-09-08 12:19:38 +0100

    add somme unit tests for page_title

---
+++ tests/Unit/HelpersTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class HelpersTest extends TestCase
+{
+    /** @test */
+    public function page_title_should_return_the_correct_title()
+    {
+        $this->assertTrue(true);
+    }
+}
